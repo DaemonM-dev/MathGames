94e643ec48f706d53036d35f21cd2865cbdd1c71
DaemonM <RavenBoxPrints@gmail.com>
2026-08-06 11:51:56 +0100

    Drawing three white boxes for gameplay elements

---
+++ src/ui_elements/box_bubble.js
@@ -0,0 +1,21 @@
+import {Vector2i} from '../constants.js';
+
+export class BoxBubble{
+    constructor(pos = null, size = null, radius = 0, color = ''){
+        this.pos = pos;
+        this.size = size;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(ctx, scaleX, scaleY){
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 7 * Math.min(scaleX, scaleY);
+
+        ctx.beginPath();
+        ctx.roundRect(this.pos.x, this.pos.y, this.size.x, this.size.y, this.radius * Math.min(scaleX, scaleY));
+        ctx.fill();
+        ctx.stroke();
+    }
+}
--- src/ui_handler.js
+++ src/ui_handler.js
@@ -1,12 +1,19 @@
 import {Game} from './game.js'
 import {Vector2i} from './constants.js'
 import { SpeechBubble } from './ui_elements/speech_bubbles.js';
+import {BoxBubble} from './ui_elements/box_bubble.js'
 
 let dialogueBox = null;
+
+let topBox = null;
+let middleBox = null;
+let bottomBox = null;
+
 let mousePressed = false;
 
 export function initUI(){
     initSpeechBubble();
+    initBoxBubble();
 }
 
 export function updateUI(deltaTime){
@@ -15,6 +22,7 @@ export function updateUI(deltaTime){
 
 export function drawUI(){
     drawSpeechBubble();
+    drawBoxBubble();
 }
 
 export function handleMouseDown(){
@@ -46,17 +54,30 @@ function initSpeechBubble(){
     dialogueBox.addMessage("This is a speech bubble with multiple messages.");
     dialogueBox.addMessage("Messages will cycle automatically.");
 }
-
 function updateSpeechBubble(deltaTime){
     if (dialogueBox && mousePressed) {
         dialogueBox.nextMessage();
         mousePressed = false; // Reset after handling
     }
 }
-
 function drawSpeechBubble(){
    if (dialogueBox) {
         dialogueBox.draw(Game.ctx, Game.scaleX, Game.scaleY);
     }
 }
-
+function initBoxBubble(){
+    console.log("Creating top box...");
+    topBox = new BoxBubble(new Vector2i(200,200), new Vector2i(200,200), 15, '#ffffff');
+    console.log("Top box created:", topBox);
+    console.log("Creating middle box...");
+    middleBox = new BoxBubble(new Vector2i(500,500), new Vector2i(200,200), 15, '#ffffff');
+    console.log("Middle box created:", topBox);
+    console.log("Creating bottom box...");
+    bottomBox = new BoxBubble(new Vector2i(800,350), new Vector2i(200,200), 15, '#ffffff');
+    console.log("Bottom box created:");
+}
+function drawBoxBubble(){
+    topBox.draw(Game.ctx,Game.scaleX, Game.scaleY);
+    middleBox.draw(Game.ctx,Game.scaleX, Game.scaleY);
+    bottomBox.draw(Game.ctx,Game.scaleX, Game.scaleY);
+}
