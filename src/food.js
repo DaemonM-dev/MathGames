ff0ade88b4ad3b477dd5465e7ec262b6e92b58ed
DaemonM <RavenBoxPrints@gmail.com>
2026-08-12 16:23:05 +0100

    minor selecting adjustments

--- src/food.js
+++ src/food.js
@@ -1,27 +1,29 @@
+import { GAME_WIDTH, GAME_HEIGHT } from "./constants.js";
+
 export class FoodItem{
     constructor(texture, name, size, pos, scale, cost){
         this.texture = texture;
         this.name = name;
 
-        this.size = size;
-        this.cachedSize = size;
+        this.size = {...size};
+        this.pos = {...pos};
+        this.scale = {...scale};
 
-        this.pos = pos;
-        this.cachedPos = pos;
-
-        this.scale = scale;
-        this.cachedScale = scale;
+        this.cachedSize = {...size};
+        this.cachedPos = {...pos};
 
         this.selected = false;
         this.cost = cost;
     }
 
     update(scale, ctx, deltaTime){
-        this.size = { x: this.cachedSize.x * scale.x, y: this.cachedSize.y * scale.y };
+
+        if(scale.x !== this.scale.x || scale.y !== this.scale.y){this.scale = scale;}
+        this.size = { x: this.cachedSize.x * this.scale.x, y: this.cachedSize.y * this.scale.y };
     }
 
     draw(ctx){
-        ctx.drawImage(this.texture, this.pos.x, this.pos.y, this.size.x, this.size.y);
+        ctx.drawImage(this.texture, this.pos.x * this.scale, this.pos.y * this.scale.y, this.size.x, this.size.y);
     }
 
     checkForCollision(mousePos){
@@ -42,9 +44,11 @@ export class FoodItem{
     }
 
     move(mousePos){
-        if(this.selected){this.pos = {
-            x:mousePos.x - (this.size.x / 2),
-            y:mousePos.y - (this.size.y / 2)}
-        };
+        if(this.selected){
+            this.pos = {
+                x: mousePos.x - (this.size.x / 2.0),
+                y: mousePos.y - (this.size.y / 2.0)
+            }; 
+        }
     }
 }
