5ce97c586286d376016ce77067c7617bc87c3bcf
DaemonM <RavenBoxPrints@gmail.com>
2026-09-02 15:10:56 +0100

    Minor tweaks after evolution branch merge

--- src/gameplay/gameplay.js
+++ src/gameplay/gameplay.js
@@ -72,9 +72,7 @@ export class Gameplay {
         this.initFoods(assets);
         this.initDialogue();
 
-        // Temp
         this.feedbackIndex = Math.floor(Math.random() * 3);
-        //
     }
     initScene(assets){
         this.scene.init(assets);
